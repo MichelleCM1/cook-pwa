de2859a33530b0a0e940a7909700cb18f6ead4a2
MICHELLE CAROLINE MARTINS DOS SANTOS <michelle.cmsantos1@senacsp.edu.br>
2024-09-12 21:27:29 -0300

    app my cook for config pwa

---
+++ src/components/ingredients/index.tsx
@@ -0,0 +1,27 @@
+import { Ingredient, ingredientsProps } from "@/components/ingredient"
+import { ScrollView } from "react-native"
+import { services } from "@/services"
+import { styles } from './styles';
+
+type Props ={
+    ingredients: ingredientsProps[]
+}
+
+export function Ingredients( { ingredients }: Props) {
+    return (
+        <ScrollView
+            contentContainerStyle={styles.container}
+            style={styles.container}
+            showsHorizontalScrollIndicator={true}
+            horizontal={true}
+        >
+            {ingredients.map( (ingredient) => (
+                <Ingredient
+                    key={ingredient.name}
+                    name={ingredient.name}
+                    image={`${services.storage.imagePath}/${ingredient.image}`}
+                />    
+            ))}
+        </ScrollView>
+    )
+}
